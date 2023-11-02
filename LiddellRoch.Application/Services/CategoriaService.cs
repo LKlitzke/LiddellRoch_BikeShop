d7bbec365d82062c03650ba320a7866d1e4f213f
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2023-11-02 17:02:34 -0300

    Adding Service files in Application project

---
+++ LiddellRoch.Application/Services/CategoriaService.cs
@@ -0,0 +1,57 @@
+﻿using LiddellRoch.Application.Services.Interfaces;
+using LiddellRoch.DataAccess.Repository.Interfaces;
+using LiddellRoch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiddellRoch.Application.Services
+{
+    public class CategoriaService : ICategoriaService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoriaService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void CreateCategory(Categoria categoria)
+        {
+            _unitOfWork.Categoria.Add(categoria);
+            _unitOfWork.Save();
+        }
+        public void UpdateCategory(Categoria categoria)
+        {
+            _unitOfWork.Categoria.Update(categoria);
+            _unitOfWork.Save();
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            try
+            {
+                Categoria categoria = GetCategoryById(id);
+
+                _unitOfWork.Categoria.Remove(categoria);
+                _unitOfWork.Save();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<Categoria> GetAllCategories()
+        {
+            return _unitOfWork.Categoria.GetAll().ToList();
+        }
+
+        public Categoria GetCategoryById(int id)
+        {
+            return _unitOfWork.Categoria.GetFirstOrDefault(u => u.Id == id);
+        }
+    }
+}
---
+++ LiddellRoch.Application/Services/Interfaces/ICategoriaService.cs
@@ -0,0 +1,18 @@
+﻿using LiddellRoch.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LiddellRoch.Application.Services.Interfaces
+{
+    public interface ICategoriaService
+    {
+        IEnumerable<Categoria> GetAllCategories();
+        Categoria GetCategoryById(int id);
+        void CreateCategory(Categoria categoria);
+        void UpdateCategory(Categoria categoria);
+        bool DeleteCategory(int id);
+    }
+}
