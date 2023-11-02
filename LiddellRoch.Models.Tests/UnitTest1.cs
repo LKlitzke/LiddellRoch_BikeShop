44e4d74446f39f5502e12333574276e7c9413360
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2023-11-02 00:54:05 -0300

    Updating Models.Tests project

--- LiddellRoch.Models.Tests/UnitTest1.cs
+++ LiddellRoch.Models.Tests/UnitTest1.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
 namespace LiddellRoch.Models.Tests
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public UnitTest1(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
         [Fact]
         public void Test1()
         {
