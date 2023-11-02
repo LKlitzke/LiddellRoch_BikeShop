cf60fd3002bcc9216414a900e8f3bd916e74efe0
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2023-11-02 00:54:13 -0300

    Updating Utility project

---
+++ LiddellRoch.Utility/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LiddellRoch.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public MailJetSettings mailJetSettings;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            mailJetSettings = _configuration.GetSection("MailJet").Get<MailJetSettings>();
+
+            MailjetClient client = new MailjetClient(mailJetSettings.ApiKey, mailJetSettings.SecretKey);
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+            .Property(Send.FromEmail, "lksilva1703@protonmail.com")
+            .Property(Send.FromName, "lksilva-noreply")
+            .Property(Send.Subject, subject)
+            .Property(Send.HtmlPart, htmlMessage)
+            .Property(Send.Recipients, new JArray {
+                new JObject {
+                    {"Email", email}
+                }
+            });
+            await client.PostAsync(request);
+
+        }
+    }
+}
