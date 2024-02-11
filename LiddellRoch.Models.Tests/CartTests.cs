5803a96a81375eef144977d9c2fc44b5737bb7bd
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-02-10 21:12:16 -0300

    Adding unit tests for Bike, Cart and User

---
+++ LiddellRoch.Models.Tests/CartTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LiddellRoch.Models.Tests
+{
+    public class CartTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public CartTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public void QuantidadeMustBeGreaterThanZero_ReturnsFalse()
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras { Quantidade = 0 };
+
+            // Act
+            var isValid = carrinho.Quantidade > 0;
+
+            // Assert
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void PrecoMustBeGreaterThanZero_ReturnsFalse()
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras { Preco = -1 };
+
+            // Act
+            var isValid = carrinho.Preco > 0;
+
+            // Assert
+            Assert.False(isValid);
+        }
+    }
+}
---
+++ LiddellRoch.Models.Tests/UserTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LiddellRoch.Models.Tests
+{
+    public class UserTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public UserTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public void CreateUserWithMasks_ReturnsOk()
+        {
+            // Arrange
+            ApplicationUser user = new ApplicationUser()
+            {
+                Nome = "tester",
+                Empresa = null,
+                CodigoPostal = "29260000",
+                Endereco = "RUA 12301920192",
+                Cidade = "Sundays Martins",
+                PhoneNumber = "12345678901"
+            };
+
+            // Act
+            var propriedade = typeof(ApplicationUser).GetProperty("Nome");
+            var isAttribRequired = propriedade.IsDefined(typeof(RequiredAttribute), true);
+
+            // Assert
+            Assert.Equal(8, user.CodigoPostal.Length);
+            Assert.True(user.PhoneNumber.Length >= 11);
+            Assert.True(user.Endereco.Length >= 10);
+            Assert.True(isAttribRequired);
+            Assert.Null(user.Empresa);
+        }
+
+        [Fact]
+        public void LockUserAccess_ReturnsLockout()
+        {
+            // Arrange
+            ApplicationUser user = new ApplicationUser()
+            {
+                Nome = "tester 2",
+                CriadoEm = DateTime.Now
+            };
+
+            // Act
+            user.LockoutEnabled = true;
+            user.LockoutEnd = DateTime.Now.AddDays(30);
+
+            // Assert
+            Assert.True(user.LockoutEnabled);
+        }
+    }
+}
